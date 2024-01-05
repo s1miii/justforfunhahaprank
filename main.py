c5a82723cfb08f05371a60231c1ad2550ad07853
Simi <84782994+s1miii@users.noreply.github.com>
2024-01-06 00:40:49 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+import time
+
+sleep_times = [3,4,1.5,2,0.75,4,4,3,2,3,4]
+sentence = "MintedSucces MintedSucces ErrorTxpool MintedSucces MintedSucces MintedSucces MintedSucces MintedSucces GassFeeError GasFeeError GassFeeError"
+for sleep_time,word in zip(sleep_times,sentence.split()):
+    print(word)
+    time.sleep(sleep_time)   
+

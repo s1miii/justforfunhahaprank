0b7747a1f76dbbe06ca1ddc2c95a6ce0edbf15b4
Simi <84782994+s1miii@users.noreply.github.com>
2024-01-12 22:24:45 +0700

    Update botseiminting.py

--- botseiminting.py
+++ botseiminting.py
@@ -1,8 +1,9 @@
-8import time
+import time
+print("mau berapa kali mint lord:")
+x = input()
 
 sleep_times = [3,4,1.5,2,0.75,4,4,3,2,3,4]
 sentence = "MintedSucces MintedSucces ErrorTxpool MintedSucces MintedSucces MintedSucces MintedSucces MintedSucces GassFeeError GasFeeError GassFeeError"
 for sleep_time,word in zip(sleep_times,sentence.split()):
     print(word)
-    time.sleep(sleep_time)   
-
+    time.sleep(sleep_time)

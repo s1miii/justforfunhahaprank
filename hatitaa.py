fa53259c00b64f669492daad86c376175854c31e
Simi <84782994+s1miii@users.noreply.github.com>
2024-01-13 11:18:01 +0700

    Create hatitaa.py

---
+++ hatitaa.py
@@ -0,0 +1,35 @@
+import turtle
+s=turtle.Screen().bgcolor('grey')
+t=turtle.Turtle()
+t.speed(3)
+t.width(12)
+
+def Curve():
+    for i in range(200):
+        t.right(1)
+        t.forward(1)
+
+def heart():
+    t.color('red','red')
+    t.begin_fill()
+    t.left(140)
+    t.forward(113)
+    Curve()
+    t.left(120)
+    Curve()
+    t.forward(112)
+    t.end_fill()
+
+heart()
+t.pencolor('grey')
+t.penup()
+t.goto(0,170)
+t.pendown()
+
+for broken in range(3):
+    t.left(75)
+    t.forward(40)
+    t.right(65)
+    t.forward(45)
+
+turtle.done()

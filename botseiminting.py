ba87a3166aaa299fbdc6c4d7237f885f7a7f3d0e
Simi <84782994+s1miii@users.noreply.github.com>
2024-02-28 20:34:42 +0700

    Update botseiminting.py

--- botseiminting.py
+++ botseiminting.py
@@ -1,9 +1,9 @@
 import time
-print("mau berapa kali mint lord:")
+print("file ktp:")
 x = input()
 
 sleep_times = [3,4,1.5,2,0.75,4,4,3,2,3,4]
-sentence = "MintedSucces MintedSucces ErrorTxpool MintedSucces MintedSucces MintedSucces MintedSucces MintedSucces GassFeeError GasFeeError GassFeeError"
+sentence = "RegisterDataSavepharse.txt ErrorThread RegisterDataSavepharse.txt RegisterDataSavepharse.txt RegisterDataSavepharse.txt RegisterDataSavepharse.txt RegisterDataSavepharse.txt RegisterDataSavepharse.txt RegisterDataSavepharse.txt RegisterDataSavepharse.txt"
 for sleep_time,word in zip(sleep_times,sentence.split()):
     print(word)
     time.sleep(sleep_time)
